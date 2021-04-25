d21f68816054028aa1e4ecc8a86c0ee747d9cb92
Jim Geist <info@jimgeist.com>
2021-04-25 11:26:45 -0400

    v2.00: 'Number Facts' and 'Deck of Cards' both refactored to use async / wait instead of .then() and .catch() promise methods with callbacks. The refactored code also includes enhancements to the code submitted for 31-01-12 Node / Async exercises.

--- NumberFacts/static/asyncExercises.js
+++ NumberFacts/static/asyncExercises.js
@@ -2,56 +2,64 @@
     NUMBERS API RELATED FUNCTIONS
 */
 
-function callNumbersAPI(favNbr, fx) {
-
-    /*
-        Call the numbers api at http://numbersapi.com/favNbr
-        for trivia about the number
-    */
-
-    const ROOT_API = 'http://numbersapi.com/';
-    const config = {
-        headers: { "Content-Type": "application/json" }
-    }
-
-    let numberPromise = axios.get(`${ROOT_API}${favNbr}`, config);
-
-    numberPromise
-        .then(data => fx(data, "success"))
-        .catch(err => fx(err, "failure"));
-
+const API_ROOT = 'http://numbersapi.com/';
+const API_CONFIG = {
+    headers: { "Content-Type": "application/json" }
 }
 
-function callNumbersAPIMulti(favNbr, nbrOfFacts, fx) {
+async function callNumbersAPI(favNbr, nbrOfFacts, fx) {
 
     /*
         Call the numbers api at http://numbersapi.com/favNbr
-        for trivia about the number.
+        for trivia about the number(s) in favNbr. Multiple numbers
+        are possible for favNbr as long as they are separated by
+        a comma. 
 
-        nbrOfFacts is the number of facts to return.
+        nbrOfFacts is the number of facts to return for the number.
+        When multiple numbers are in favNbr, nbrOfFacts must be 1. 
+        
+        The function that is calling callNumbersAPI (this function) 
+        must ensure that there are no spaces in favNbr -- only digits
+        and commas -- and that nbrOfacts is 1 when more than one 
+        number is in favNbr.
 
-        Yes, this function (callNumbersAPIMulti) and callNumbersAPI 
-        could get refactored into one function. 
+        fx is the name of the function that processes the 
+        results returned from the api.
+
     */
-
-    const ROOT_API = 'http://numbersapi.com/';
-    const config = {
-        headers: { "Content-Type": "application/json" }
-    }
 
     const nbrFacts = []
 
     for (let i = 0; i < nbrOfFacts; i++) {
-        nbrFacts.push(axios.get(`${ROOT_API}${favNbr}`, config));
+        nbrFacts.push(axios.get(`${API_ROOT}${favNbr}`, API_CONFIG));
     }
 
-    Promise.all(nbrFacts)
-        .then(data => fx(data, "success-m"))
-        .catch(err => fx(err, "error"))
+    try {
+        const response = await Promise.all(nbrFacts);
+
+        // We will get nbrFact data responses back. Only check the status
+        //  for the first one in the response data array.
+        if (response[0].status === 200) {
+            if (response.length == 1) {
+                fx(response[0].data, "success");
+            } else {
+                fx(response, "success-m");
+            }
+
+        } else {
+            fx(response, "error");
+        }
+    }
+    catch (error) {
+        fx(error, "error");
+    }
 
 }
 
-/** processForm: get data from form and make AJAX call to our API. */
+
+/* setFavNbrFormFields will handle populating the form with number data from the api call.
+   Form messaging and error fields are cleared when data is ''.
+*/
 
 function setFavNbrFormFields(data, type) {
 
@@ -63,16 +71,15 @@ function setFavNbrFormFields(data, type) {
 
             let msg;
 
-            if (typeof (data["data"]) === "string") {
-                msg = `${data["data"]}`;
+            if (typeof (data) === "string") {
+                msg = `${data}`;
 
             } else {
                 msg = '';
                 let delim = '';
-                for (let key in data["data"]) {
-                    msg = `${msg}${delim}${data["data"][key]}`;
+                for (let key in data) {
+                    msg = `${msg}${delim}${data[key]}`;
                     delim = "<br>";
-                    //$(`#${key}-err`).text(indata[key][0]);
                 }
             }
 
@@ -90,22 +97,19 @@ function setFavNbrFormFields(data, type) {
                 $("#favnbr-trivia").html(`<br>${msg}<br>`);
 
             } else {
-
                 // we have an error. Put data in the field's error.
                 $("#fav-nbr-err").text(data);
-
             }
 
         }
 
     } else {
-
+        // Clear the trivia message and error messages.
         $("#favnbr-trivia").html("<br><br><br>");
         $("#fav-nbr-err").text("");
         $("#nbr-of-facts-err").text("");
 
     }
-
 
 }
 
@@ -114,21 +118,23 @@ function processFavNbrForm(evt) {
 
     evt.preventDefault();
 
+    // Clear the form fields
     setFavNbrFormFields('', '');
 
     let favNbr = ($("#fav-nbr").val().trim()).replaceAll(' ', '');
     if (favNbr.length > 0) {
-        nbrOfFacts = $("#nbr-of-facts").val();
+        nbrOfFacts = 1 * ($("#nbr-of-facts").val());
+
         if (nbrOfFacts > 1) {
             // make sure there is only one number in favNbr.
             if (favNbr.indexOf(",") > -1) {
                 $("#nbr-of-facts-err").text(`Ignored ${nbrOfFacts} - Multiple facts are not available for mulitiple numbers.`);
-                callNumbersAPI(favNbr, setFavNbrFormFields);
+                callNumbersAPI(favNbr, 1, setFavNbrFormFields);
             } else {
-                callNumbersAPIMulti(favNbr, nbrOfFacts, setFavNbrFormFields);
+                callNumbersAPI(favNbr, nbrOfFacts, setFavNbrFormFields);
             }
         } else {
-            callNumbersAPI(favNbr, setFavNbrFormFields);
+            callNumbersAPI(favNbr, 1, setFavNbrFormFields);
         }
 
     } else {
--- README.md
+++ README.md
@@ -1,39 +1,35 @@
-# sb_31-01-12_AsynchronousCodeInJavaScript
+# sb_31-02-10_AsynchronousCodeInJavaScript
 
-Please go to [jimgeist.github.io/sb_31-01-12_AsynchronousCodeInJavaScript](https://jimgeist.github.io/sb_31-01-12_AsynchronousCodeInJavaScript/) to execute the code via gitpages.
+Please go to [jimgeist.github.io/sb_31-02-10_AsynchronousCodeInJavaScript](https://jimgeist.github.io/sb_31-02-10_AsynchronousCodeInJavaScript/) to execute the code via gitpages.
 
 ## Assignment Details
 ## Part 1: Number Facts
-1. Make a request to the [Numbers API](http://numbersapi.com/) to get a fact about your favorite number. 
+Make a request to the [Numbers API](http://numbersapi.com/) to get a fact about a favorite number, multiple facts (some may repeat) about a favorite number, or one fact per number when multiple numbers are specified at once. 
 
-1. Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
+`async` and `await` were used instead of using promises with `.then()` / `.catch()` callbacks.
 
-1. Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
-
-**Number Facts does not work on git hub pages because of mixed content. The numbers api is not available via https.**
+**PLEASE NOTE: Number Facts does not work on git hub pages because of mixed content. The numbers api is not available via https.**
 
 **Enhancements**
 
-- I am not sure if this counts as an enhancement, but the same form is used for all 3 options and each are correctly handled with JavaScript.
+- The same form is used for all 3 options and each are correctly handled with JavaScript.
 - For multiple numbers in a single request, any spaces are removed from the favorite number(s) field before processing.
+- The same API function is called for both mulitiple facts about one number and for one fact for each number when multiple numbers are entered for favorite numbers.
 
 
 ## Part 2: Deck of Cards
 
-It is funny that the Deck of Cards API exists!
+The exercise involved building an html page that allows for the draw of cards from the deck. 
 
-The real piece of this exercise was building the html page that allows for the draw of cards from the deck. I thought to use localStorage to use the same deck_id instead of a new deck each time. It was possible, but the completed code required a decent amount of refactoring since it could not continue until the promise was satisfied, and it seemed incorrect to insert async and wait.  
+`async` and `await` were used instead of using promises with `.then()` / `.catch()` callbacks.
+
+I thought to use localStorage to use the same deck_id instead of a new deck each time. It was possible, but the completed code required a decent amount of refactoring since it could not continue until the promise was satisfied, and it seemed incorrect to insert async and wait.  
 
 
 ### ENHANCEMENTS
-- 'Card Me' button is disabled when remaining cards is zero (. . . I know - lame!). 
+- A DeckOfCards class was created with methods to shuffle a deck, shuffle a new deck, deal cards, get the deck id from local storage and save the deck id to local storage. 
+- 'Card Me' button is disabled when remaining cards is zero. 
 
 
 ### DIFFICULTIES 
-Forgetting how asynchronous code works and not thinking 4th dimensionally!
-
-When stepping through code in a debugger, the promise does not execute right away. Without `async` and `wait`, the 'main' code will continue. This presented an issue as I attempted to stray away from always using a new deck each time and instead thought to write the deck id to local storage and use the local storage deck id. A shuffle in the page load event tests the deck id and a new deck id is obtained if the shuffle fails. 
-
-Writing this description out made me realize the pieces that required refactoring to get it all to work. Needless to say, no local storage, and we get a new deck every time the program runs. Sad how long it takes to add code, but how quickly it can get removed (and no, I did not try this in a new branch which would have made the abort even quicker!).
-
-And while on git hub, interesting factoid -- 'main' is used because they no longer wanted to use 'master'. ['Master', ‘Slave’ and the Fight Over Offensive Terms in Computing](https://www.nytimes.com/2021/04/13/technology/racist-computer-engineering-terms-ietf.html)
+Forgetting to add `this.` before any method calls when the method call is made from within the class! And forgetting to set the class deckId to the deck_id returned when a new deck was used.
